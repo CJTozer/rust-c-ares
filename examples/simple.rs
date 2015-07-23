8dc44745166e2a983a23818603d73b6c025c8e10
Chris Tozer <christopherjtozer@gmail.com>
2015-07-23 22:29:44 +0000

    Ok, not Futures

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,9 @@
 // Simple example using `Futures`.
-#![feature(std_misc)]
 extern crate c_ares;
 
-use std::sync::{mpsc, Future};
+use std::sync::mpsc;
 
-// TODO - commonize.
+// TODO - commonize with the other example(s).
 fn print_a_results(result: Result<c_ares::AResults, c_ares::AresError>) {
     println!("");
     match result {
@@ -33,6 +32,7 @@ impl Resolver {
         // Create a c_ares::Channel.
         let mut options = c_ares::Options::new();
         options
+            //.set_flags(c_ares::flags::STAYOPEN | c_ares::flags::EDNS)
             .set_timeout(500)
             .set_tries(3);
         let ares_channel = c_ares::Channel::new(dummy_callback, options) // Cheating on the channel
@@ -42,15 +42,15 @@ impl Resolver {
         Resolver { ares_channel: ares_channel }
     }
 
-    fn a_query_as_future(&mut self, name: &str) -> Future<Result<c_ares::AResults, c_ares::AresError>> {
+    fn a_query_as_future(&mut self, name: &str) -> Box<Fn() -> Result<c_ares::AResults, c_ares::AresError>> {
         // Make the query.
         let (tx, rx) = mpsc::channel();
         self.ares_channel.query_a(name, move |results| {
             tx.send(results).unwrap();
         });
         
-        // Return a `Future` waiting to receive the result.
-        Future::spawn(move || {
+        // Return a closure that waits to receive the result.
+        Box::new(move || {
             rx.recv().unwrap()
         })
     }
@@ -59,11 +59,11 @@ impl Resolver {
 fn main() {
     // Perform a query, getting the result as a future.
     let mut resolver = Resolver::new();
-    let future_results = resolver.a_query_as_future("apple.com");
+    let get_results = resolver.a_query_as_future("apple.com");
 
     // Do some other stuff here while we wait
     // ...
 
     // Wait for and print the results
-    print_a_results(future_results.into_inner());
+    print_a_results(get_results());
 }
